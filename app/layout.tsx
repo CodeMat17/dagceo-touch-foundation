93c6680585a0ebca63e8ac87a19e15495a172fdb
Matthew <codemat.biz@gmail.com>
2024-03-23 15:01:48 +0100

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "../components/theme-provider";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 import Footer from "@/components/Footer";
 import { Suspense } from "react";
 import { CircleDotDashed } from "lucide-react";
@@ -27,19 +28,20 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute='class'>
           <ClerkProvider>
-          <section>
-            <NavBar />
-            <Suspense
-              fallback={
-                <div className='pt-20 flex items-center justify-center'>
-                  <CircleDotDashed className='w-16 h-16 font-semibold text-2xl' />
-                </div>
-              }>
-              {children}
-            </Suspense>
+            <section>
+              <NavBar />
+              <Suspense
+                fallback={
+                  <div className='pt-20 flex items-center justify-center'>
+                    <CircleDotDashed className='w-16 h-16 font-semibold text-2xl' />
+                  </div>
+                }>
+                {children}
+              </Suspense>
 
-            <Footer />
-          </section>
+              <Footer />
+            </section>
+            <Toaster />
           </ClerkProvider>
         </ThemeProvider>
       </body>
