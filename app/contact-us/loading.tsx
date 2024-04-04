7a8652a973518b81114fd8115d13b86b6117cd60
Matthew <codemat.biz@gmail.com>
2024-04-04 16:10:22 +0100

    updated

---
+++ app/contact-us/loading.tsx
@@ -0,0 +1,12 @@
+import { CircleDotDashed } from "lucide-react";
+
+const loading = () => {
+  return (
+    <div className='flex flex-col w-full h-screen items-center gap-4 px-5 pt-32'>
+      <CircleDotDashed className='font-semibold text-4xl w-32 h-32 animate-spin' />
+      <p>loading</p>
+    </div>
+  );
+};
+
+export default loading;
